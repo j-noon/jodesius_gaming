918b555cd09f0a20245a653866964cf5495ba203
joseph <pj9epkfc@students.codeinstitute.net>
2025-07-09 20:22:05 +0100

    database secrey key and vars set up

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(ifo9kweez7ihj06g+!vrkpx)sva*0xs&_huq44%@=ygc9ce_l'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
