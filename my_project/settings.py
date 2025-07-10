67072c47195d4268a7d012ba1665c4d940d277d8
joseph <pj9epkfc@students.codeinstitute.net>
2025-07-10 20:23:55 +0100

    css linked through core home and base html

--- my_project/settings.py
+++ my_project/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'staticfiles',  # my global CSS/JS/images
+]
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
