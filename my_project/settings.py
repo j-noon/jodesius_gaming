bfd3bf32e27849ea6b139a6bbb3e92ff61b17d84
joseph <pj9epkfc@students.codeinstitute.net>
2025-07-14 17:14:34 +0100

    Fix static files for Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -147,11 +147,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static',  # my global CSS/JS/images
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Where collectstatic puts files
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # Your custom static folders
+
+# WhiteNoise configuration
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
