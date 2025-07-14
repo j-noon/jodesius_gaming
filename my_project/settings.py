c44c86cba7779531ddad369e34eaa801f5f31618
joseph <pj9epkfc@students.codeinstitute.net>
2025-07-14 17:04:07 +0100

    Fix static files for Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -150,7 +150,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',  # my global CSS/JS/images
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
